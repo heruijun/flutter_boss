9d0967d52e98ba94f3b5a8732e34d67808c5a767
daiwj <1298300385@qq.com>
2018-03-23 10:25:09 +0800

    底部tab 文字增加选中和非选中状态颜色

--- lib/app/component/icon_tab.dart
+++ lib/app/component/icon_tab.dart
@@ -1,24 +1,37 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-const double _kTextAndIconTabHeight = 53.0;   // 导航高度
-const double _kMarginBottom = 3.0;            // 图标与文字的间隔
+const double _kTextAndIconTabHeight = 53.0; // 导航高度
+const double _kMarginBottom = 3.0; // 图标与文字的间隔
 
-class IconTab extends StatelessWidget {
+class IconTab extends StatefulWidget {
 
   const IconTab({
     Key key,
     this.text,
     this.icon,
-  }): assert(text != null || icon != null),
-        super(key: key);
+    this.color,
+  })
+    : assert(text != null || icon != null || color != null),
+      super(key: key);
 
   final String text;
   final String icon;
+  final Color color;
+
+  @override
+  State<StatefulWidget> createState() {
+    return new IconTabState();
+  }
+}
+
+class IconTabState extends State<IconTab> {
 
   Widget _buildLabelText() {
-    return new Text(text, softWrap: false, overflow: TextOverflow.fade);
+    return new Text(widget.text,
+      softWrap: false,
+      overflow: TextOverflow.fade,
+      style: new TextStyle(color: widget.color),
+    );
   }
 
   @override
@@ -27,19 +40,19 @@ class IconTab extends StatelessWidget {
 
     double height = _kTextAndIconTabHeight;
     Widget label = new Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          new Container(
-            child: new Image(
-              image: new AssetImage(this.icon),
-              height: 30.0,
-              width: 30.0,
-            ),
-            margin: const EdgeInsets.only(bottom: _kMarginBottom),
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        new Container(
+          child: new Image(
+            image: new AssetImage(widget.icon),
+            height: 30.0,
+            width: 30.0,
           ),
-          _buildLabelText()
-        ]
+          margin: const EdgeInsets.only(bottom: _kMarginBottom),
+        ),
+        _buildLabelText()
+      ]
     );
 
     return new SizedBox(
@@ -50,4 +63,5 @@ class IconTab extends StatelessWidget {
       ),
     );
   }
+
 }
